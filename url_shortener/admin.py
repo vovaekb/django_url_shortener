09b685cdfe92e1dda31246499061f6c09b7512b1
Vladimir Privalov <vovaprivalov@gmail.com>
2023-03-24 20:34:27 +0400

    Add data models

--- url_shortener/admin.py
+++ url_shortener/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Link, Visit
 
-# Register your models here.
+admin.site.register(Link)
+admin.site.register(Visit)
--- url_shortener/models.py
+++ url_shortener/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+class VisitStatistics(models.Model):
+    """Model definition for VisitStatistics."""
+    total_visits = models.IntegerField()
+
+    def __str__(self):
+        """Unicode representation of VisitStatistics."""
+        pass
+
+
+class Link(models.Model):
+    """Model definition for Link."""
+    full_url = models.CharField(max_length=256)
+    slug = models.CharField(max_length=6, unique=True)
+    created_at = models.DateTimeField(auto_now=False, auto_now_add=False)
+    statistics = models.OneToOneField(VisitStatistics, on_delete=models.CASCADE)
+
+    def __str__(self):
+        """Unicode representation of Link."""
+        return self.name
+
+
+class Visit(models.Model):
+    """Model definition for Visit."""
+    datetime = models.DateTimeField(auto_now=False, auto_now_add=False)
+    ip_address = models.CharField(max_length=50)
+    link = models.ForeignKey(Link, on_delete=models.CASCADE)
+
+    def __str__(self):
+        """Unicode representation of Visit."""
+        pass
+
