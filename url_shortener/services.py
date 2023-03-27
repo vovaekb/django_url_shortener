f9a94e2db057c0849aa8a690b397caccb48b9717
Vladimir Privalov <vovaprivalov@gmail.com>
2023-03-27 17:47:03 +0400

    Add doсstring

--- url_shortener/services.py
+++ url_shortener/services.py
@@ -4,10 +4,20 @@ from django.utils import timezone
 
 
 def get_short_url_hash():
+    """Generate token for short url from upper case and lower case letters and digits
+    
+    Return: short url token
+    """
     random_hash = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(6))
     return random_hash
 
 def get_ip_address(request):
+    """Get user IP address from HTTP request
+    
+    Keyword arguments:
+    request -- HTTP request object HttpRequest
+    Return: ip address
+    """
     user_ip = request.META.get('HTTP_X_FORWARDED_FOR')
     if user_ip:
         ip = user_ip.split(',')[0]
--- url_shortener/views.py
+++ url_shortener/views.py
@@ -7,6 +7,19 @@ from .forms import UrlForm
 
 
 def short_url_view(request):
+    """
+    Display a form for entering url address to make short version on GET request and create new :model:`url_shortener.Link` from form data on POST request.
+
+    **Context**
+    ``form``
+        A form :form:`url_shortener.UrlForm`.
+    ``error_message``
+        Message for validation error. 
+
+    **Template:**
+
+    :template:`url_shortener/url_form.html`
+    """
     form = UrlForm
     if request.method == 'POST':
         if form.is_valid():
@@ -33,18 +46,37 @@ def short_url_view(request):
     return render(request, 'url_form.html', context)
 
 def redirect_url(request, token):
+    """
+    Redirect user to initial url addres when user request the resource using short link (domain name + url token)
+    and update the visits counter:
+    - get user IP address
+    - create new :model:`url_shortener.Visit`.
+    - update :model:`url_shortener.VisitStatistics`.
+
+    **Redirect:**
+
+    :field:`url_shortener.Link.full_url`
+    """
     link = Link.objects.filter(slug=token).first()
-    print('link: ', link.full_url)
     ip_address = get_ip_address(request)
     Visit.objects.create(ip_address=ip_address, link=link)
     statistics = link.statistics
     statistics.total_visits = statistics.total_visits + 1
     statistics.save()
-    print('total_visits: ', statistics.total_visits)
-    return HttpResponse('redirect_url', 200)
-    # return redirect(link.full_url)
+    return redirect(link.full_url)
 
 def statistics_view(request):
+    """
+    Display statistics on visiting urls stored in daat base - list of :model:`url_shortener.VisitStatistics`.
+
+    **Context**
+    ``links``
+        A list of :model:`url_shortener.Link`.
+
+    **Template:**
+
+    :template:`url_shortener/statistics.html`
+    """
     links = Link.objects.all()
     context = { 'links': links }
     return render(request, 'statistics.html', context)
